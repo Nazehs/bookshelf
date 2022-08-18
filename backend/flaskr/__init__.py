9329b91065a2239740c890e359ec0b6e7728eb95
Nazeh Abel <talentednazeh@gmail.com>
2022-08-19 00:37:22 +0100

    Add error handling

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -8,13 +8,24 @@ from models import setup_db, Book
 
 BOOKS_PER_SHELF = 8
 
+def paginate_books(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start =  (page - 1) * BOOKS_PER_SHELF
+    end = start + BOOKS_PER_SHELF
 
+    books = [book.format() for book in selection]
+    current_books = books[start:end]
+
+    return current_books
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+
+
+
 
     # CORS Headers
     @app.after_request
@@ -43,36 +54,68 @@ def create_app(test_config=None):
         })
     @app.route("/books/<int:book_id>", methods=['PATCH'])
     def update_book_rating(book_id, rating):
-        book = Book.query.get(book_id)
-        book.rating = rating
-        book.update()
-        return jsonify({
-            'success': True
-        })
+            try:
+                book = Book.query.get(book_id)
+                book.rating = rating
+                book.update()
+                return jsonify({
+                    'success': True
+                })
+            except:
+                abort(404)
     
     @app.route("/books/<int:book_id>", methods=['DELETE'])
     def delete_book(book_id):
-        book = Book.query.get(book_id)
-        book.delete()
-        return jsonify({
-            'success': True,
-            'deleted': book_id,
-            'books': [book.format() for book in Book.query.all()],
-            'total_books': Book.query.count()
-        })
+        try:
+            book = Book.query.get(book_id)
+            book.delete()
+            return jsonify({
+                'success': True,
+                'deleted': book_id,
+                'books': [book.format() for book in Book.query.all()],
+                'total_books': Book.query.count()
+            })
+        except:
+            abort(404)
    
     @app.route('/books', methods=['POST'])
     def create_book():
-        title = request.json.get('title', None)
-        author = request.json.get('author', None)
-        rating = request.json.get('rating', None)
-        book = Book(title=title, author=author, rating=rating)
-        book.insert()
+            try:
+                    title = request.json.get('title', None)
+                    author = request.json.get('author', None)
+                    rating = request.json.get('rating', None)
+                    book = Book(title=title, author=author, rating=rating)
+                    book.insert()
+                    return jsonify({
+                        'success': True,
+                        'created': book.id,
+                        'books': [book.format() for book in Book.query.all()],
+                        'total_books': Book.query.count()
+                    })
+            except Exception as e:
+                abort(422)
+            
+    @app.errorhandler(404)
+    def not_found(error):
         return jsonify({
-            'success': True,
-            'created': book.id,
-            'books': [book.format() for book in Book.query.all()],
-            'total_books': Book.query.count()
-        })
-  
+            "success": False, 
+            "error": 404,
+            "message": "Not found"
+            }), 404
+        
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+        "success": False, 
+        "error": 422,
+        "message": "unprocessable"
+        }), 422
+        
+    @app.errorhandler(403)
+    def bad_request(error):
+        return jsonify({
+        "success": False, 
+        "error": 403,
+        "message": "unprocessable"
+        }), 403
     return app
